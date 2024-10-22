38c36231d650e6f0e57c2e7ff20ab35c6be86532
Julius Peter Ntale <juliusntale30@gmail.com>
2024-10-22 22:06:21 +0300

    Prpper Picture Postion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "361b1df1cba1da54"; }
+/******/ 		__webpack_require__.h = function() { return "0e125c014a944a16"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,14 +12,19 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/ntale.png"  alt="Thats me"
-  priority className="object-contain" />
-        </div>
+       <div className="h-full overflow-scroll flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+       {/* IMAGE CONTAINER */}
+        <div className="relative w-full h-1/2 lg:w-1/2 lg:h-full flex-shrink-0">
+      <Image
+        src="/ntale.png"
+        fill
+        alt="Julius Ntale - Professional Portfolio"
+        priority
+        className="object-contain"
+      />
+    </div>
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        <div className="w-full h-1/2 flex z-10 flex-col gap-8  lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
             Crafting Digital Experiences, Designing Tomorrow.
