874734147ef5203f46f8559b61a70028c088e3ad
Julius Peter Ntale <juliusntale30@gmail.com>
2024-10-22 22:28:37 +0300

    Changes to be committed: 	modified:   .next/app-build-manifest.json 	modified:   .next/cache/webpack/client-development/5.pack.gz 	modified:   .next/cache/webpack/client-development/index.pack.gz.old

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8a1bd67f3a83ae5d")
+/******/ 		__webpack_require__.h = () => ("d6639de063bc7c30")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e125c014a944a16"; }
+/******/ 		__webpack_require__.h = function() { return "31475bbfb97e5895"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
