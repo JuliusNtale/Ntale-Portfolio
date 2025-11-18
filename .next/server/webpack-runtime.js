bdb7b21725786a2d3cfa1443c63c24b2f89894f0
Julius Peter Ntale <juliusntale30@gmail.com>
2025-11-18 11:27:00 +0300

    chore: Update webpack cache files and modify server manifests for improved routing and security

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("de6595119cb65e77")
+/******/ 		__webpack_require__.h = () => ("93fab349593f98df")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
